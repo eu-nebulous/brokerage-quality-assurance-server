6f520a3469829108e660ee08a3b02daff55b6573
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-05-20 12:33:07 +0300

    Update BrokerageQualityAssurance.java

--- src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
+++ src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BrokerageQualityAssurance{
 	public static void main(String[] args) {
 		
-
+		OntologyConnection.getInstance();
 		SpringApplication.run(BrokerageQualityAssurance.class, args);
 	}
 
