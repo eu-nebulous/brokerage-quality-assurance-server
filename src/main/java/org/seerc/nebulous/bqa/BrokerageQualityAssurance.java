6c0d25fed9442941cdecce3063dd3468993af635
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-11-28 13:54:14 +0200

    Update BrokerageQualityAssurance.java (#24)

--- src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
+++ src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BrokerageQualityAssurance{
 	public static void main(String[] args) {
-		
+		System.out.println("THIS IS A TEST LINE \n\n");
 		OntologyConnection.getInstance("http://nebulous-ontology-server:80");
 		EXNConnection.getInstance();
 		SpringApplication.run(BrokerageQualityAssurance.class, args);
