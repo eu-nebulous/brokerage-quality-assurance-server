ba1e5646c23cdef8e96cd4905df9958d8b46c837
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-11-28 12:49:06 +0200

    Update BQAServeUI.java

--- src/main/java/org/seerc/nebulous/bqa/rest/BQAServeUI.java
+++ src/main/java/org/seerc/nebulous/bqa/rest/BQAServeUI.java
@@ -1,13 +1,13 @@
-package org.seerc.nebulous.bqa.rest;
+// package org.seerc.nebulous.bqa.rest;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-public class BQAServeUI {
-	@GetMapping({"/index", "/"})
-	public String greeting(@RequestParam(name="appId", required = true) String applicationId, @RequestParam(name="nonce", required = true) String nonce ) {
-		return "index";
-	}
-}
+// @Controller
+// public class BQAServeUI {
+// 	@GetMapping({"/index", "/"})
+// 	public String greeting(@RequestParam(name="appId", required = true) String applicationId, @RequestParam(name="nonce", required = true) String nonce ) {
+// 		return "index";
+// 	}
+// }
