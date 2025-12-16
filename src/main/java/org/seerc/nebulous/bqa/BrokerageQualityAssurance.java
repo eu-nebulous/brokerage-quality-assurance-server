cdfd811ca2edaecba48cedce2cdfcdd72821b4d3
ebarmpas <56959801+ebarmpas@users.noreply.github.com>
2025-12-16 17:22:31 +0200

    Fix ont (#42)
    * Fixed problematic ontology connection
    
    * Fixed ontology connection
    
    ---------
    
    Co-authored-by: vaggbarmpas@gmail.com <evbarmpas@seerc.org>

--- src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
+++ src/main/java/org/seerc/nebulous/bqa/BrokerageQualityAssurance.java
@@ -4,25 +4,15 @@ import org.seerc.nebulous.bqa.rest.EXNConnection;
 import org.seerc.nebulous.bqa.rest.OntologyConnection;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class BrokerageQualityAssurance{
 	public static void main(String[] args) {
 		
-		System.out.println("Tryingo request to ontology server");
-		System.out.println(String.format("Base URL: \"%s\"", args[0]));
-		
-		WebClient c = WebClient.create(args[0]);
-		String ret = c.get().uri("/").retrieve().bodyToMono(String.class).block();
-		System.out.println(String.format("Request  to ontology server done %s",ret));	
-
-		System.out.println("Ontology URL: " + args[0]);
-		OntologyConnection oc = OntologyConnection.getInstance( args[0]);
-		int res = oc.countInstances("Thing");
-		System.out.println(String.format("oc.countInstances %d",res));
+		System.out.println("Beginning setup...");
+		OntologyConnection.getInstance(args[0]);
 		EXNConnection.getInstance();
-		System.out.println("Set up Complete.");
+		System.out.println("Set up Complete...");
 		SpringApplication.run(BrokerageQualityAssurance.class, args);
 	}
 
